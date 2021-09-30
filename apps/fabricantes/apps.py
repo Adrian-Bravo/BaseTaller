987f92a2da2d558ef281d14f6bfa923b6c3d874b
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-09-30 01:25:59 -0500

    actualice name de fabricantes en apps

--- apps/fabricantes/apps.py
+++ apps/fabricantes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FabricantesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fabricantes'
+    name = 'apps.fabricantes'
