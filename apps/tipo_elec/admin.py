eb99f51cd12aa500fdc38109eaacc43cc0c56ac5
Adrian <acamilobravo@uniguajira.edu.co>
2021-09-30 01:42:02 -0500

    Corregí admin en tipo_elec

--- apps/tipo_elec/admin.py
+++ apps/tipo_elec/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from apps.tipo_elec.models import Tipo_elec
+from apps.tipo_elec.models import Tipos_elec
 
 # Register your models here.
 
-admin.site.register(Tipo_elec)
+admin.site.register(Tipos_elec)
