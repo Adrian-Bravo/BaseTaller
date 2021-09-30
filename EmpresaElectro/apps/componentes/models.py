308fcf355912541d6161c4f55ebf2f52009a7394
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-09-30 00:58:59 -0500

    agregue clase de componentes

--- EmpresaElectro/apps/componentes/models.py
+++ EmpresaElectro/apps/componentes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.aparatos.models import Aparatos
 
 # Create your models here.
+
+class Componentes(models.Model):
+    nombre = models.CharField(max_length=60)
+    especificaciones = models.CharField(max_length=300)
+    cantidad = models.IntegerField(default=0)
+    precio = models.BigIntegerField()
+    aparatos = models.ForeignKey(Aparatos, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
