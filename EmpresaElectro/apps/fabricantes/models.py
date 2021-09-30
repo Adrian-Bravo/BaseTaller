9ce0f63981fda1820f5c1dd442a67cc483cd5072
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-09-30 01:00:14 -0500

    agregue clase de fabricantes

--- EmpresaElectro/apps/fabricantes/models.py
+++ EmpresaElectro/apps/fabricantes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.componentes.models import Componentes
 
 # Create your models here.
+
+class Fabricantes(models.Model):
+    nombre = models.CharField(max_length=60)
+    domicilio = models.CharField(max_length=200)
+    componentes = models.ForeignKey(Componentes, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
