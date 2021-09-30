9c6d85c3210962a6d1a3e5bf79e20870d15f5a2a
Ivonne-cas <ivonneximenacaste@gmail.com>
2021-09-30 00:57:18 -0500

    agregue clase de aparatos

--- EmpresaElectro/apps/aparatos/models.py
+++ EmpresaElectro/apps/aparatos/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-from apps.aparatos.models import Aparatos
-# Create your models here.
-class Tipos_elec(models.Model):
-    nombre = models.CharField(max_length=60)
-    caracteristicas = models.TextField()
-    aparatos = models.ForeignKey(Aparatos, on_delete=models.CASCADE)
+
+class Aparatos(models.Model):
+    descripcion = models.CharField(max_length=300)
 
     def __str__(self):
-        return self.nombre
+        return self.descripcion
--- EmpresaElectro/apps/tipo_elec/models.py
+++ EmpresaElectro/apps/tipo_elec/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from apps.aparatos.models import Aparatos
 # Create your models here.
+class Tipos_elec(models.Model):
+    nombre = models.CharField(max_length=60)
+    caracteristicas = models.TextField()
+    aparatos = models.ForeignKey(Aparatos, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
