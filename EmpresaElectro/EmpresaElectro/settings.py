34e52f5be7bc58e1a422e87df421bf6c5ff773b4
edanielmosocte <edanielmoscote@uniguajira.edu.co>
2021-09-30 01:09:13 -0500

    registre la aplicacion componentes en el settings

--- EmpresaElectro/EmpresaElectro/settings.py
+++ EmpresaElectro/EmpresaElectro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.aparatos.apps.AparatosConfig',
+    'apps.componentes.apps.ComponentesConfig',
 ]
 
 MIDDLEWARE = [
